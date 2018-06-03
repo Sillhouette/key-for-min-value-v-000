3a879f7fd87ca46ee1ed450041058fc2d8fc9abb
Richard Melchior <raustin.melchior@gmail.com>
2018-06-03 05:23:24 +0000

    automatically backed up by learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -7,12 +7,12 @@ def key_for_min_value(name_hash)
   name_hash.collect do |key, value|
     if lowest_value == 0
       lowest_value = value
+      lowest_key = key
     end
     if value < lowest_value
       lowest_value = value;
       lowest_key = key;
     end
   end
-  lowest_value = 0
     return lowest_key;
 end
